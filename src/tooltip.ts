e8d6c11023ab1f286afdacb5883eeac5718c42d6
Bishwajit Jha <ajitjha393@gmail.com>
2020-08-09 12:05:30 +0530

    "Added tooltipIcon  inside connectedCallBack lifecycle method"

--- src/tooltip.ts
+++ src/tooltip.ts
@@ -3,6 +3,13 @@ class Tooltip extends HTMLElement {
 		super()
 		console.log('Tooltip being created!')
 	}
+
+	// Whenever we need to access the DOM
+	connectedCallback() {
+		const tooltipIcon = document.createElement('span')
+		tooltipIcon.textContent = ' (?) '
+		this.appendChild(tooltipIcon)
+	}
 }
 
 customElements.define('bisu-tooltip', Tooltip)
