2724911cf6aed2ff19b03fb7a806d2d4a421ba8a
Bishwajit Jha <ajitjha393@gmail.com>
2020-08-10 19:44:51 +0530

    Finished basic overall styling

--- src/tooltip.ts
+++ src/tooltip.ts
@@ -14,15 +14,22 @@ class Tooltip extends HTMLElement {
 		<style>
 
 			div {
+				font-weight: normal;
 				background-color: black;
 				color: white;
 				position: absolute;
-				z-index: 10
+				top: 1.5rem;
+				left: 0.75rem;
+				z-index: 10;
+				padding: 0.15rem;
+				border-radius: 3px;
+				box-shadow: 1px 1px 6px rgba(0,0,0,0.26)
 
 			}
 
 			:host(.important) {
 				background-color: var(--color-primary,#ccc);
+				padding: 0.2rem;
 			}
 
 			:host-context(p) {
@@ -30,7 +37,7 @@ class Tooltip extends HTMLElement {
 			}
 
 			::slotted(.highlight) {
-				border-bottom: 2px dotted red;
+				border-bottom: 1px dotted red;
 			}
 
 			.icon {
