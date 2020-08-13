54eda7bd6fa9eddadf69d64815fda3358ff7377a
Bishwajit Jha <ajitjha393@gmail.com>
2020-08-13 15:17:23 +0530

    Added basic Backdrop Styling

--- src/modal.ts
+++ src/modal.ts
@@ -5,7 +5,19 @@ class Modal extends HTMLElement {
 		this.attachShadow({ mode: 'open' })
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
-			
+
+				<style>
+					#backdrop {
+						position: fixed;
+						top: 0;
+						left: 0;
+						width: 100%;
+						height: 100vh;
+						background: rgba(0,0,0,0.5);
+
+					}
+				</style>
+
 				<div id="backdrop"></div>
 				<div id="modal"></div>
 
