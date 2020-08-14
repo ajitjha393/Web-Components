c348ef83bb43264a248f199da68489df517052c8
Bishwajit Jha <ajitjha393@gmail.com>
2020-08-14 15:38:22 +0530

    Generated js filefrom ts for hosting and usage over cdn

---
+++ build/confirm-link.js
@@ -0,0 +1,14 @@
+"use strict";
+class ConfirmLink extends HTMLAnchorElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.addEventListener('click', event => {
+            if (!confirm('Do you really want to be redirected ?')) {
+                event.preventDefault();
+            }
+        });
+    }
+}
+customElements.define('bisu-confirm-link', ConfirmLink, { extends: 'a' });
